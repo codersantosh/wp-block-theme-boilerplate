d40f13fbc0ffdf2521ba44d42834dd56f7021189
codersantosh <codersantosh@gmail.com>
2024-09-21 15:04:46 +0545

    Removed class id

--- patterns/featured-section-2.php
+++ patterns/featured-section-2.php
@@ -15,7 +15,7 @@
 <div class="wp-block-group alignfull has-default-background-color has-background" style="padding-top:80px;padding-bottom:80px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"60px"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%"} -->
 <div class="wp-block-column" style="flex-basis:50%"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/featured-img-1.png" alt="" class="wp-image-73"/></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/featured-img-1.png" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
